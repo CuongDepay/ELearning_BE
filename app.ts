89c1a738e4cc19e92103a6af3af48ad91b074ed6
CuongDepay <cuongdepay2002@gmail.com>
2024-04-25 21:35:51 +0700

    feat: user register

--- app.ts
+++ app.ts
@@ -3,7 +3,8 @@ import express, { Request, Response, NextFunction } from "express";
 export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { ErrorMiddleware } from "./middleware/error";
+import { ErrorMiddleware } from './middleware/error';
+import userRouter from "./routes/user.route";
 
 // body parser
 app.use(express.json({limit: "50mb"}));
@@ -14,6 +15,9 @@ app.use(cookieParser());
 app.use(cors({
     origin: process.env.ORIGIN
 }));
+
+// routes
+app.use("/api/v1", userRouter);
 
 app.get('/test', (req: Request, res: Response, next:NextFunction) => {
     res.status(200).json({
@@ -27,3 +31,5 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
     err.statusCode = 404;
     next(err);
 });
+
+app.use(ErrorMiddleware);
