47d1d580b504507f03958c3982f1feeda31cd0a0
CuongDepay <cuongdepay2002@gmail.com>
2024-05-03 14:27:10 +0700

    authentication

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,7 +1,11 @@
 import express from 'express';
-import { registrationUser } from '../controllers/user.controller';
+import { registrationUser, activateUser, loginUser, logoutUser } from '../controllers/user.controller';
+import { isAuthenticated } from '../middleware/auth';
 const userRouter = express.Router();
 
 userRouter.post('/registration', registrationUser);
+userRouter.post('/activate-user', activateUser);
+userRouter.post('/login', loginUser);
+userRouter.post('/logout', isAuthenticated,logoutUser);
 
 export default userRouter;
