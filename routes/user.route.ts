352ece388405023f4082dd9f86bf5aa4e5df001a
CuongDepay <cuongdepay2002@gmail.com>
2024-05-04 13:04:44 +0700

    social auth and profile

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,11 +1,16 @@
 import express from 'express';
-import { registrationUser, activateUser, loginUser, logoutUser } from '../controllers/user.controller';
+import { registrationUser, activateUser, loginUser, logoutUser, updateAccessToken, getUserInfo, socialAuth, updateUserInfo } from '../controllers/user.controller';
 import { isAuthenticated } from '../middleware/auth';
 const userRouter = express.Router();
 
 userRouter.post('/registration', registrationUser);
 userRouter.post('/activate-user', activateUser);
 userRouter.post('/login', loginUser);
-userRouter.post('/logout', isAuthenticated,logoutUser);
+userRouter.get('/logout', isAuthenticated,logoutUser);
+userRouter.get('/refresh', updateAccessToken);
+userRouter.get('/me', isAuthenticated, getUserInfo);
+userRouter.post('/social-auth', socialAuth);
+userRouter.put('/update-user-info', isAuthenticated, updateUserInfo);
+
 
 export default userRouter;
